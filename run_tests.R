9cc6958e6267ac2026562a8dcd45c95e98e590b6
ap-- <andreas@poehlmann.io>
2020-05-17 23:15:41 +0200

    [tests] R 4.0 regression in getOption
    R 4.0 has no default browser set ?!?

--- run_tests.R
+++ run_tests.R
@@ -5,6 +5,11 @@
 library(testthat)
 library(shinytest)
 
+if (getOption("browser") == "") {
+    # fix R 4.0 not getting default browser...
+    options(browser = "xdg-open")
+}
+
 test_that("all systems nominal", {
   # by default runs all scripts in ./tests/
   expect_pass(testApp(".", compareImages = FALSE))
