7507c4e4da002d728de26848bebe14b18d456c07
ap-- <andreas@poehlmann.io>
2020-05-18 10:43:08 +0200

    [app] move example data to www

--- tests/test_data_upload_page.R
+++ tests/test_data_upload_page.R
@@ -1,6 +1,6 @@
 app <- ShinyDriver$new("../", loadTimeout = 10000)
 app$snapshotInit("test_data_upload_page")
 
-app$uploadFile(`csv_feature_counts-csvfile` = "input_data/featurecounts.csv") # <-- This should be the path to the file, relative to the app's tests/ directory
-app$uploadFile(`csv_column_data-csvfile` = "input_data/meta.csv") # <-- This should be the path to the file, relative to the app's tests/ directory
+app$uploadFile(`csv_feature_counts-csvfile` = "../www/example_data/featurecounts.csv") # <-- This should be the path to the file, relative to the app's tests/ directory
+app$uploadFile(`csv_column_data-csvfile` = "../www/example_data/meta.csv") # <-- This should be the path to the file, relative to the app's tests/ directory
 app$snapshot()
